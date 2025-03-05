bc371170829a961f10f7edff17e1cf13c35faecf
Dieu <cdinh1078@gmail.com>
2025-03-05 15:13:30 +0700

    CRUD done

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index'
+import CRUDService from '../services/CRUDService';
 
 let getHomePage = async (req, res) => {
     try {
@@ -9,13 +10,62 @@ let getHomePage = async (req, res) => {
     } catch (e) {
         console.log(e);
     }
-
-
 }
+
 let getAboutPage = (req, res) => {
     return res.render('about.ejs')
 }
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send("post crud from srver");
+}
+
+let displayCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser();
+    return res.render('displayCRUD.ejs', { dataTable: data })
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        //chech user not found
+
+
+        return res.render("editCRUD.ejs", { user: userData })
+    } else {
+        return res.send("User not found");
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render("displayCRUD.ejs", { dataTable: allUsers })
+}
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('Delete user succed');
+    }
+    return res.send(' user not found ');
+}
+
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayCRUD: displayCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD
 }
